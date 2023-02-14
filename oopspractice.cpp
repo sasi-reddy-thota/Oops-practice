369b014ed531b9cd175412a877b8843334a575b1
sasi <thotasasikumarreddy@gmail.com>
2023-02-14 11:50:02 +0530

    Friend class Practice

--- oopspractice.cpp
+++ oopspractice.cpp
@@ -1,33 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Account{
-    int AccountNo;
-    string name;
+class A{
+    int x;
 
     public:
-    static int count_accounts;
+    A(int x){
+        this->x=x;
+    }
+    friend class B;
 
-        Account(int AccountNo,string name){
-            this->AccountNo=AccountNo;
-            this->name=name;
-            count_accounts+=1;
-        }
-
-        void getDetails(){
-            cout<<"Account No: "<<AccountNo<<" "<<"\t"<<"Name: "<<name<<"\n";
-        }
 };
-int Account::count_accounts=0;
+
+class B{
+    public:
+    void display(A a){
+        cout<<a.x;
+    }
+};
 
 int main(){
-    Account *A1= new Account(123456543,"Sasi Kumar");
-    Account *A2= new Account(123456543,"Sasi Kumar");
-    Account *A3= new Account(123456543,"Sasi Kumar");
-    Account *A4= new Account(123456543,"Sasi Kumar");
-    A1->getDetails();
-
-    cout<<Account::count_accounts<<endl;
-
+    A *a= new A(5);
+    B *b=new B;
+    b->display(*a);
     return 0;
 }
